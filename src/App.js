a6a2d95863b3852ab77eeb0d94cafa5828cac71a
Oyku <oykgokcek@gmail.com>
2023-01-09 01:43:04 +0300

    template done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { dummyData } from "./components/dummyData";
+import Karakter from "./components/Karakter";
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const Chars = styled.div`
+    display: flex;
+    background-color: blue;
+    width: auto;
+  `;
+
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <div>
+        <Karakter data={data} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+// gender: male
+// Height: 172
+// Mass: 77
+// BirthYear: 19BBY
+// Eye Color: blue
+// Hair Color: blond
+// Skin Color: fair
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,36 @@
 // Karakter bileşeniniz buraya gelecek
+
+import React from "react";
+import styled from "styled-components";
+
+export default function Karakter(props) {
+  const { data } = props;
+
+  // const CharProperties = style.div`
+
+  // `;
+  const Chars = styled.div`
+    display: flex;
+    background-color: blue;
+    width: auto;
+  `;
+
+  return (
+    <div>
+      {data.map((person) => (
+        <Chars>
+          <button>{person.name}</button>
+          <div className="Properties">
+            <p>{person.gender}</p>
+            <p>{person.height}</p>
+            <p>{person.mass}</p>
+            <p>{person.birth_year}</p>
+            <p>{person.eye_color}</p>
+            <p>{person.hair_color}</p>
+            <p>{person.skin_color}</p>
+          </div>
+        </Chars>
+      ))}
+    </div>
+  );
+}
